a4f6ebcb3dade78be5dd3db4a421d65ec2fd6ae5
Swetha Ganapathi Raman <swethag04@gmail.com>
2024-05-02 10:52:26 -0700

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='End to end ML project to classify wafers',
+    author='swethag04',
+    license='MIT',
+)
